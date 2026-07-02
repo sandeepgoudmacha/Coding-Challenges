58aa6413514fc48a20782530f4b31b94f3b912c4
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2026-07-02 17:39:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Permutation/next-permutation.cpp
+++ Difficulty: Medium/Next Permutation/next-permutation.cpp
@@ -1,7 +1,27 @@
 class Solution {
   public:
+
+  
     void nextPermutation(vector<int>& arr) {
         // code here
-        next_permutation(arr.begin(), arr.end());
+        int n=arr.size();
+         int j=n-1;
+        while(j>0 && arr[j-1]>=arr[j]){
+            j--;
+        }
+        if(j == 0){
+         reverse(arr.begin(),arr.end());
+         return;
+        }
+        int pivot=j-1;
+        j=n-1;
+        while(j>pivot){
+            if(arr[j]>arr[pivot]){
+                break;
+            }
+            j--;
+        }
+        swap(arr[pivot], arr[j]);
+        reverse(arr.begin()+pivot+1, arr.end());
     }
 };
