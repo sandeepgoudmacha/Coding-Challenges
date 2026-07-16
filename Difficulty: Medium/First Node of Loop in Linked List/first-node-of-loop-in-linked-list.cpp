efd02c9978d7e5e1afccc9318a0acb94bec9bcd6
sandeepgoudmacha <143279752+sandeepgoudmacha@users.noreply.github.com>
2026-07-16 15:37:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First Node of Loop in Linked List/first-node-of-loop-in-linked-list.cpp
@@ -0,0 +1,35 @@
+/*
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int cycleStart(Node* head) {
+        // code here
+        if(head==NULL || head->next==NULL) return -1;
+        Node* slow=head;
+        Node* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow->data;
+            }
+        }
+        return -1;
+    }
+};
